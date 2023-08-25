a7675330dbd453699985a43c9db21f21b5959fda
Saitanuja <sai.vachiva@gdcsg.com>
2023-08-25 08:38:48 -0400

    Added the Terraform code to create the Azure Resources

--- modules/Azure-Infra/variables.tf
+++ modules/Azure-Infra/variables.tf
@@ -1,5 +1,92 @@
 
-variable "user_names" {
+variable "usernames" {
   type    = list(string)
   default = ["AD-User1", "AD-User2", "AD-User4", "AD-User5"]
 }
+
+variable "resource_group_location" {
+  type        = string
+  description = "Location for all resources."
+  default     = "eastus"
+}
+
+variable "resource_group_name_prefix" {
+  type        = string
+  description = "Prefix of the resource group name that's combined with a random ID so name is unique in your Azure subscription."
+  default     = "rg"
+}
+
+variable "vault_name" {
+  type        = string
+  description = "The name of the key vault to be created. The value will be randomly generated if blank."
+  default     = ""
+}
+
+variable "key_name" {
+  type        = string
+  description = "The name of the key to be created. The value will be randomly generated if blank."
+  default     = ""
+}
+
+variable "sku_name" {
+  type        = string
+  description = "The SKU of the vault to be created."
+  default     = "standard"
+  validation {
+    condition     = contains(["standard", "premium"], var.sku_name)
+    error_message = "The sku_name must be one of the following: standard, premium."
+  }
+}
+
+variable "key_permissions" {
+  type        = list(string)
+  description = "List of key permissions."
+  default     = ["List", "Create", "Delete", "Get", "Purge", "Recover", "Update"]
+}
+
+variable "secret_permissions" {
+  type        = list(string)
+  description = "List of secret permissions."
+  default     = ["Set"]
+}
+
+variable "key_type" {
+  description = "The JsonWebKeyType of the key to be created."
+  default     = "RSA"
+  type        = string
+  validation {
+    condition     = contains(["EC", "EC-HSM", "RSA", "RSA-HSM"], var.key_type)
+    error_message = "The key_type must be one of the following: EC, EC-HSM, RSA, RSA-HSM."
+  }
+}
+
+variable "key_ops" {
+  type        = list(string)
+  description = "The permitted JSON web key operations of the key to be created."
+  default     = ["decrypt", "encrypt", "sign", "unwrapKey", "verify", "wrapKey"]
+}
+
+variable "key_size" {
+  type        = number
+  description = "The size in bits of the key to be created."
+  default     = 2048
+}
+
+variable "msi_id" {
+  type        = string
+  description = "The Managed Service Identity ID. If this value isn't null (the default), 'data.azurerm_client_config.current.object_id' will be set to this value."
+  default     = null
+}
+
+variable "tenant_id" {
+  type        = string
+  description = "The Tenant Id"
+  default     = "40f32e4e-08ac-49c5-b2fc-a71c2a4c2fbe"
+}
+
+variable "object_id" {
+  type        = string
+  description = "The Object Id"
+  default     = "acbe24ba-c58c-4ad5-ab1e-431ad2ea8af2"
+}
+
