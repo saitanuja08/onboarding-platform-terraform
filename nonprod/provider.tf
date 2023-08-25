ed696af3fb6e5e3e42fbc73766a05a85a7fc2e61
Saitanuja <sai.vachiva@gdcsg.com>
2023-08-25 08:54:25 -0400

    Added the Terraform code to create the Azure Resources

--- nonprod/provider.tf
+++ nonprod/provider.tf
@@ -10,4 +10,8 @@ terraform {
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
     features {}
+    client_id       = var.azure_client_id
+    client_secret   = var.azure_client_secret
+    subscription_id = var.azure_subscription_id
+    tenant_id       = var.azure_tenant_id
 }
