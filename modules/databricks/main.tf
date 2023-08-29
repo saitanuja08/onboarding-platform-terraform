6183d8b6d4feecec27709a463c60f58f7dcf1302
saitanuja08 <138033514+saitanuja08@users.noreply.github.com>
2023-08-29 08:25:36 -0400

    Update main.tf

--- modules/databricks/main.tf
+++ modules/databricks/main.tf
@@ -1,3 +1,21 @@
+locals {
+  default_policy = {
+    "dbus_per_hour" : {
+      "type" : "range",
+      "maxValue" : 10
+    },
+    "autotermination_minutes" : {
+      "type" : "fixed",
+      "value" : 20,
+      "hidden" : true
+    },
+    "custom_tags.Team" : {
+      "type" : "fixed",
+      "value" : var.team
+    }
+  }
+}
+
 data "databricks_user" "usr" {
   user_name = "me@example.com" # This is just a placeholder. Please update the value to an actual username before running the code.
 }
@@ -26,6 +44,19 @@ resource "databricks_entitlements" "me" {
   user_id                    = data.databricks_user.me.id
   allow_cluster_create       = true
   allow_instance_pool_create = true
+}
+
+resource "databricks_cluster_policy" "fair_use" {
+  name       = "${var.team} cluster policy"
+  definition = jsonencode(merge(local.default_policy, var.policy_overrides))
+}
+
+resource "databricks_permissions" "can_use_cluster_policyinstance_profile" {
+  cluster_policy_id = databricks_cluster_policy.fair_use.id
+  access_control {
+    group_name       = var.team
+    permission_level = "CAN_USE"
+  }
 }
 
 
