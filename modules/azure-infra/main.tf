f7742db4e0096eccee6d695ef7785e5de6f4d36e
Saitanuja <sai.vachiva@gdcsg.com>
2023-09-21 08:41:19 -0400

    Created a workflow to create selective resources using Terraform

--- modules/azure-infra/main.tf
+++ modules/azure-infra/main.tf
@@ -1,24 +1,24 @@
 # Create the Resource Group
-resource azurerm_resource_group rg {
+resource azurerm_resource_group resource_group {
   name                         = var.resource_group_name
   location                     = var.location
 }
 
 # Create VNet
-resource "azurerm_virtual_network" "poc_vnet" {
+resource "azurerm_virtual_network" "vnet" {
   name                = var.vnet_name
   address_space       = var.vnet_cidr
   location            = var.location
   resource_group_name = var.resource_group_name
 
-  depends_on = [azurerm_resource_group.rg] 
+  depends_on = [azurerm_resource_group.resource_group] 
 }
 
 # Create Private Subnet1
 resource "azurerm_subnet" "private_subnet_1" {
   name                 = var.private_subnet2_name
   resource_group_name  = var.resource_group_name
-  virtual_network_name = azurerm_virtual_network.poc_vnet.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = var.private_subnet1_cidr
 }
 
@@ -26,7 +26,7 @@ resource "azurerm_subnet" "private_subnet_1" {
 resource "azurerm_subnet" "private_subnet_2" {
   name                 = var.private_subnet2_name
   resource_group_name  = var.resource_group_name
-  virtual_network_name = azurerm_virtual_network.poc_vnet.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = var.private_subnet2_cidr
 }
 
@@ -34,7 +34,7 @@ resource "azurerm_subnet" "private_subnet_2" {
 resource "azurerm_subnet" "public_subnet_1" {
   name                 = var.public_subnet1_name
   resource_group_name  = var.resource_group_name
-  virtual_network_name = azurerm_virtual_network.poc_vnet.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = var.public_subnet1_cidr
 }
 
@@ -42,12 +42,12 @@ resource "azurerm_subnet" "public_subnet_1" {
 resource "azurerm_subnet" "public_subnet_2" {
   name                 = var.public_subnet2_name
   resource_group_name  = var.resource_group_name
-  virtual_network_name = azurerm_virtual_network.poc_vnet.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = var.public_subnet2_cidr
 }
 
 # Create Route Table 
-resource "azurerm_route_table" "public_route_table" {
+resource "azurerm_route_table" "route_table" {
   name                = var.route_table_name
   location            = var.location
   resource_group_name = var.resource_group_name
@@ -58,19 +58,19 @@ resource "azurerm_route_table" "public_route_table" {
     next_hop_type  = "Internet"
   }
 
-  depends_on = [azurerm_resource_group.rg]
+  depends_on = [azurerm_resource_group.resource_group]
 }
 
 # Associate Route Table with Public Subnet1
 resource "azurerm_subnet_route_table_association" "public_subnet_association_1" {
   subnet_id      = azurerm_subnet.public_subnet_1.id
-  route_table_id = azurerm_route_table.public_route_table.id
+  route_table_id = azurerm_route_table.route_table.id
 }
 
 # Associate Route Table with Public Subnet2
 resource "azurerm_subnet_route_table_association" "public_subnet_association_2" {
   subnet_id      = azurerm_subnet.public_subnet_2.id
-  route_table_id = azurerm_route_table.public_route_table.id
+  route_table_id = azurerm_route_table.route_table.id
 }
 
 #data "azuread_client_config" "current" {}
@@ -96,46 +96,46 @@ resource "azurerm_subnet_route_table_association" "public_subnet_association_2" 
 #}
 
 # Create the Log Analytics Workspace
-resource "azurerm_log_analytics_workspace" "la" {
+resource "azurerm_log_analytics_workspace" "log_analytics_workspace" {
   name                = var.log_analytics_workspace_name
   location            = var.location
   resource_group_name = var.resource_group_name
 
-  depends_on = [azurerm_resource_group.rg]
+  depends_on = [azurerm_resource_group.resource_group]
 }
 
 # Create the Storage Account
-resource "azurerm_storage_account" "sa" {
+resource "azurerm_storage_account" "storage_account" {
   name                     = var.storage_account_name
   resource_group_name      = var.resource_group_name
   location                 = var.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 
-  depends_on = [azurerm_resource_group.rg]
+  depends_on = [azurerm_resource_group.resource_group]
 }
 
 # Create the Blob Storage in the Storage Account
-resource "azurerm_storage_container" "blob" {
+resource "azurerm_storage_container" "storage_container" {
   name                  = var.storage_container_name
-  storage_account_name  = azurerm_storage_account.sa.name
+  storage_account_name  = azurerm_storage_account.storage_account.name
   container_access_type = "private"
 }
 
 # Create Key Vault
-resource "azurerm_key_vault" "vault" {
+resource "azurerm_key_vault" "key_vault" {
   name                       = var.key_vault_name
   location                   = var.location
   resource_group_name        = var.resource_group_name
   tenant_id                  = var.tenant_id
   sku_name                   = var.sku_name
 
-  depends_on = [azurerm_resource_group.rg]
+  depends_on = [azurerm_resource_group.resource_group]
 }
 
 # Create Key Vault Policy
-resource "azurerm_key_vault_access_policy" "policy" {
-  key_vault_id = azurerm_key_vault.vault.id
+resource "azurerm_key_vault_access_policy" "key_vault_access_policy" {
+  key_vault_id = azurerm_key_vault.key_vault.id
   tenant_id    = var.tenant_id
   object_id    = var.object_id
 
@@ -150,8 +150,8 @@ data "azuread_service_principal" "sp" {
 }
 
 # Create Key Vault Access Policy for the Service Principal
-resource "azurerm_key_vault_access_policy" "sp" {
-  key_vault_id = azurerm_key_vault.vault.id
+resource "azurerm_key_vault_access_policy" "key_vault_service_principal_access_policy" {
+  key_vault_id = azurerm_key_vault.key_vault.id
   tenant_id    = var.tenant_id
   object_id    = data.azuread_service_principal.sp.object_id
 
